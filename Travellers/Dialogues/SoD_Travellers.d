1959313e11514b9fa1d72ced3fd5bd15709f9ac9
RoxanneSHS <cwillenbockel@aol.com>
2021-09-26 21:27:02 +0200

    v0.6

--- Travellers/Dialogues/SoD_Travellers.d
+++ Travellers/Dialogues/SoD_Travellers.d
@@ -13,3 +13,28 @@ IF~~THEN Surgeo Bridge2
 @804
 END
 IF~~THEN GOTO 2
+
+//Surgeon in Underdark
+CHAIN
+IF WEIGHT #-6~Global("SurgeonStory","GLOBAL",4)~THEN Surgeo Underd
+@810
+DO~SetGlobal("SurgeonStory","GLOBAL",5)~
+END
+++@811 + 2
+++@812 + 2
+
+//Surgeon in ToB
+CHAIN
+IF WEIGHT #-6~Global("SurgeonStory","GLOBAL",6)~THEN Surgeo Sarad1
+@805
+DO~SetGlobal("SurgeonStory","GLOBAL",7)~
+END
+++@806 + Sarad2
+++@807 + Sarad2
+++@808 + Sarad2
+
+CHAIN
+IF~~THEN Surgeo Sarad2
+@809
+END
+IF~~THEN GOTO 2
